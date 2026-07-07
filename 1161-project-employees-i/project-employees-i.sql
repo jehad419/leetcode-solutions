fdaddedea6e5c37db89e36b127e8dcb081bb076f
Md. Jehad Hasan <jehadh.workspace@gmail.com>
2026-07-07 10:49:51 +0600

    Time: 530 ms (59.25%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,9 @@
+# Solved By Jehad Hasan
+
+SELECT 
+p.project_id,
+ROUND(SUM(e.experience_years)/COUNT(*),2) AS average_years
+FROM Project p
+LEFT JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY project_id;
