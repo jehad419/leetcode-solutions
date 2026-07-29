f8ac9e599337cf44d4a2eac6b02400bdff7ae466
Md. Jehad Hasan <jehadh.workspace@gmail.com>
2026-07-29 11:57:00 +0600

    Time: 553 ms (35.40%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,20 @@
+#Solved by Jehad Hasan
+
+WITH RankedPrices AS (
+SELECT
+product_id,
+new_price,
+ROW_NUMBER() OVER (
+PARTITION BY product_id
+ORDER BY change_date DESC) AS rn
+FROM Products
+WHERE change_date <= '2019-08-16')
+
+SELECT
+p.product_id,
+COALESCE(r.new_price, 10) AS price
+FROM
+(SELECT DISTINCT product_id FROM Products) p
+LEFT JOIN RankedPrices r
+ON p.product_id = r.product_id
+AND r.rn = 1;
