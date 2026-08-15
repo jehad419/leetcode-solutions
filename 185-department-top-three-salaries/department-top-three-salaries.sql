ae9f2048987674390c716a09f6aca9b38c1db75c
Md. Jehad Hasan <jehadh.workspace@gmail.com>
2026-08-15 15:55:54 +0600

    Time: 1289 ms (30.86%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,23 @@
+#Solved by Jehad Hasan
+
+
+WITH ranked AS (
+    SELECT
+        name,
+        salary,
+        departmentId,
+        DENSE_RANK() OVER (
+            PARTITION BY departmentId
+            ORDER BY salary DESC
+        ) AS rnk
+    FROM Employee
+)
+
+SELECT
+    d.name AS Department,
+    r.name AS Employee,
+    r.salary AS Salary
+FROM ranked r
+JOIN Department d
+    ON r.departmentId = d.id
+WHERE r.rnk <= 3;
